fc680c3242904d89c4695f6021091f4b29c1fdae
Orlando Karam <okaram@spsu.edu>
2012-10-04 13:23:12 -0400

    functional sets, nice, with tests (googletest)

---
+++ functional_sets/functional_sets.h
@@ -0,0 +1,35 @@
+#ifndef _FUNCTIONAL_SETS_H_
+#define _FUNCTIONAL_SETS_H_
+#include <functional>
+
+// a set is a function, that when given an int tells me whether it's in the set
+typedef std::function<bool(int)> Set;
+
+// and a predicate is pretty much the same :) without the 'in' semantics :)
+typedef std::function<bool(int)> Predicate;
+
+// this function, when given a set, and an element, returns true if the element is in the set
+// we can simply apply the set to the element, since the set is a function :)
+bool contains(const Set&s, int elem) ;
+
+// this function creates and returns a set containing just one element
+// notice we're returning a closure ! C++ 11 supports lambdas and closures ! yipee
+Set singletonSet(int val) ;
+
+// this function returns a new set, which is the union of its two parameters
+// basically, we return a new function /closure which checks whether the element is in either of the two sets
+Set set_union(Set s1, Set s2);
+
+// this function returns a new set, which is the intersection of its two parameters
+// basically, we return a new function /closure which checks whether the element is in BOTH of the two sets
+Set set_intersection(Set s1, Set s2);
+
+// this function returns true if the predicate is true for every element in the set
+// notice that, since we can't get a list of all elements in the set, we need to use a 'grounding' set
+// in this case, we use [-1000,1000] as the grounding set
+bool foreach(Set s, Predicate p);
+
+bool exists(Set s, Predicate p) ;
+
+
+#endif
--- functional_sets/test_functional_sets.cpp
+++ functional_sets/test_functional_sets.cpp
@@ -1,45 +1,52 @@
 // test min
 #include <gtest/gtest.h>
-#include <vector>
-#include "vector_funcs_extra.h"
-#include <stdexcept>
-
+#include "functional_sets.h"
 using namespace std;
-class MyTest : public ::testing::Test{};
+class TestFunctionalSets : public ::testing::Test{
+protected:
+	Set s1,s2,s3;
+public:
+	TestFunctionalSets() {
+		s1=singletonSet(1);
+		s2=singletonSet(2);
+		s3=singletonSet(3);
+	}
+	
+};
 
 // global !! ugh :)
 int total=0;
 void add_to_total(int& i) { total+=i;}
 
-
-TEST_F(MyTest,apply_all) {
-	vector<int> v{1,3,3};
-	total=0;
-	apply_all(v,add_to_total);
-	ASSERT_EQ(7,total);
+TEST_F(TestFunctionalSets,singletonSet) {
+	ASSERT_TRUE(contains(s1,1));
+	ASSERT_TRUE(contains(s2,2));
+	ASSERT_TRUE(!contains(s1,2));
+	ASSERT_TRUE(!contains(s2,1));	
 }
 
-TEST_F(MyTest,apply_all_lambda) {
-	vector<int> v{10,3,3};
-	total=0;
-	apply_all(v,[](int& i) {total+=i; return;}); // can't capture, since type of lambda w/capture is NOT func pointer :(, not sure if portable
-	ASSERT_EQ(16,total);
+// union is a keyword in C++, so using set_union :)
+TEST_F(TestFunctionalSets,set_union) {
+	Set s=set_union(s1,s2);
+	ASSERT_TRUE(contains(s,1));
+	ASSERT_TRUE(contains(s,2));
+	ASSERT_TRUE(!contains(s,3));
+	s=set_union(s,s3);
+	ASSERT_TRUE(contains(s,1));
+	ASSERT_TRUE(contains(s,2));
+	ASSERT_TRUE(contains(s,3));
 }
 
 
-TEST_F(MyTest,map) {
-	vector<int> v {10,20,30};
-	vector<int> v2=map(v,[](int i){return 2*i;});
-	
-	ASSERT_EQ(20,v2[0]);
-	ASSERT_EQ(40,v2[1]);
-	ASSERT_EQ(60,v2[2]);
-}
+TEST_F(TestFunctionalSets,set_intersection) {
+	Set s=set_intersection(s1,s2);
+	ASSERT_TRUE(!contains(s,1));
+	ASSERT_TRUE(!contains(s,2));
 
-int add(int i,int j) {return i+j;}
-TEST_F(MyTest,reduce) {
-	vector<int> v {10,20,30};
-	ASSERT_EQ(60, reduce(v,0, [](int i,int j){return i+j;}    ));
+	s=set_intersection(set_union(s1,s2),set_union(s2,s3));
+	ASSERT_TRUE(!contains(s,1));
+	ASSERT_TRUE(contains(s,2));
+	ASSERT_TRUE(!contains(s,3));
 }
 
 int main(int argc, char **argv) 

2a8160b5d9363d1dbdf73d076ffc9e525a5c3685
Orlando Karam <okaram@spsu.edu>
2012-10-12 23:00:31 -0400

    cleanup, public version

--- conslists/conslist_tpt.h
+++ conslists/conslist_tpt.h
@@ -4,79 +4,41 @@
 template<typename T> class ConsNode;
 template<typename T> class ConsList;
 
-/*
-// will work with the new standard, need g++4.7 or better, avoiding for now
-template <typename T>
-using ConsList= std::shared_ptr<ConsNode <T> >;
-*/
-
-template<typename T>
-T car(const ConsList<T>&l);
-
-template<typename T>
-const ConsList<T>& cdr(const ConsList<T>&l);
-template<typename T>
-ConsList<T> cons(T car) ;
-
-template<typename T>
-ConsList<T> cons(T car, const ConsList<T>& cdr) ;
-
-template<class T>
-bool isEmpty(const ConsList<T>&l);
-
 template <typename T>
 class ConsList {
 public:
 	operator bool() const {return (bool)list;}
 
-private:
 	// constructors
 	ConsList(){}
-	ConsList(const T& car) {
-		list=std::make_shared<ConsNode<T> > (car);
-	}
-	ConsList(const T& car, const ConsList<T>&cdr) {
+	ConsList(const T& car, const ConsList<T>&cdr=ConsList<T>() ) {
 		list=std::make_shared<ConsNode<T> > (car,cdr);
 	}
 	
 	//data
 	std::shared_ptr<ConsNode<T> > list;
-
-	// friends
-	friend T car<>(const ConsList<T> &l);
-	friend const ConsList<T>& cdr<>(const ConsList<T> &l);
-	friend ConsList<T> cons<>(T car) ;
-	friend ConsList<T> cons<>(T car, const ConsList<T>& cdr) ;	
-	friend class ConsNode<T>;
-	friend bool isEmpty<>(const ConsList<T>&);
-
 };
 
 
 template<typename T>
 struct ConsNode {
 public:
-	ConsNode(T car, ConsList<T> cdr):_car(car),_cdr(cdr) {}
-	ConsNode(T car):_car(car){}
-private:
+	ConsNode(T car, ConsList<T> cdr=ConsList<T>() ):_car(car),_cdr(cdr) {}
+	
 	T _car;
 	ConsList<T> _cdr;
-
-	friend T car<>(const ConsList<T> &l);
-	friend const ConsList<T>& cdr<>(const ConsList<T> &l);
-	friend ConsList<T> cons<>(T car, const ConsList<T>& cdr) ;
-	friend class ConsList<T>;
-	
 };
 
 
 template<typename T>
-ConsList<T> cons(T car) {
+ConsList<T> cons(T car) 
+{
 	return ConsList<T>(car);
 }
 
 template<typename T>
-ConsList<T> cons(T car, const ConsList<T>& cdr) {
+ConsList<T> cons(T car, const ConsList<T>& cdr) 
+{
 	return ConsList<T>(car,cdr);
 }
 
@@ -97,8 +59,6 @@ const ConsList<T>& cdr(const ConsList<T>&l)
 {
 	return l.list->_cdr;
 }
-
-
 
 template<class T>
 std::ostream& operator<<(std::ostream& o, const ConsList<T>&l)
--- conslists/demo.cpp
+++ conslists/demo.cpp
@@ -3,7 +3,8 @@
 #include "conslist_tpt.h"
 
 using namespace std;
-template<class T>
+
+template<typename T>
 unsigned len(const ConsList<T>&l)
 {
 	if(!isEmpty(l))
@@ -11,13 +12,21 @@ unsigned len(const ConsList<T>&l)
 	return 1+len(cdr(l));
 }
 
+template<typename T>
+T sum(const ConsList<T>& l)
+{
+	if(isEmpty(l))
+		return 0;
+	return car(l)+sum(cdr(l));
+}
 
 int main(void)
 {
 	ConsList<int> l=cons(3,cons(4,cons(7)));
-	ConsList<int> l2=cons(9,l); // l2 shares the last few elements with l !
-	cout << car(l) << car(cdr(l)) << endl;
-	cout << len(l) << endl;
-	cout << l << endl;
+	ConsList<int> l2=cons(9,cons(5,l)); // l2 shares the last 3 elements with l 
+	ConsList<int> l3=cons(11,cdr(l2)); // l3 shares with l2, and also with l
+	cout << car(l) << " " << car(cdr(l)) << endl;
+	cout << len(l2) << " " << sum(l2) << endl;
 	cout << l2 << endl;
+	cout << l3 << endl;
 }
